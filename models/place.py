604ea25a0317325e26461094b5d99509ce9c620e
O'Brien <abucheriobrien@gmail.com>
2023-12-20 18:21:29 +0000

    DBStorage - Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.review import Review
+from models.amenity import Amenity
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -25,6 +35,9 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', cascade='all, delete',
                                backref='place')
+        amenities = relationship('Amenity', secondary='place_amenity',
+                                 back_populates='place_amenities',
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -37,3 +50,21 @@ class Place(BaseModel, Base):
             all_reviews = storage.all(Review)
             return ([review for review in all_reviews.values()
                     if review.place_id == self.id])
+
+        @property
+        def amenities(self):
+            """
+            Getter attribute that returns the list of Amenity instances
+            based on the attribute amenity_ids that contains
+            all Amenity.id linked to the Place
+            """
+            from models import storage
+
+            all_amenities = storage.all(Amenity)
+            return ([amenity for amenity in all_amenities.values()
+                    if amenity.id in self.amenity_ids])
+
+        @amenities.setter
+        def amenities(self, value):
+            if isinstance(value, Amenity):
+                self.amenity_ids.append(value.id)
