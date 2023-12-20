eb41ddf55ddb4da7e12c52b87b5adfb441bbda43
O'Brien <abucheriobrien@gmail.com>
2023-12-20 18:39:08 +0000

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Amenuitytate Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -12,5 +11,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
+    place_amenities = relationship('Place', secondary='place_amenity',
                                    back_populates='amenities')
