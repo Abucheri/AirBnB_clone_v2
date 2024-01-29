a11f442b4ead0604bd7be50772b2bbad446b69a6
O'Brien <abucheriobrien@gmail.com>
2024-01-29 01:36:41 +0000

    HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Remove the current SQLAlchemy Session after each request."""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Display a HTML page similar to 6-index.html,
+    with additional filters.
+    """
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
